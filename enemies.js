5d71e21daf3f740df299eba37df3cf17ec3b3751
Vojtik1 <it2126@sspu-opava.cz>
2023-01-23 23:25:44 +0100

    DEN 2

--- enemies.js
+++ enemies.js
@@ -21,6 +21,7 @@ class Enemy {
         if (this.x + this.width < 0) this.markedForDeletion = true;
     }
     draw(context){
+        if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
         context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y , this.width, this.height);
     }
 }
@@ -60,8 +61,32 @@ export class GroundEnemy extends Enemy {
         this.maxFrame = 1;
        
     }
+    
 }
 
 export class ClimbingEnemy extends Enemy {
-    
+    constructor(game){
+        super();
+        this.game = game;
+        this.width = 120;
+        this.height - 144;
+        this.x = this.game.width;
+        this.y =  Math.random() * this.game.height * 0.5;
+        this.image = document.getElementById('enemy_spider_big');
+        this.speedX = 0;
+        this.speedY = Math.random() > 0.5 ? 1 : -1;
+        this.maxFrame = 5;
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        if (this.y > this.game.height - this.height - this.game.groundMargin) this.speedY *= -1;
+        if (this.y < -this.height) this.markedForDeletion = true;
+    }
+    draw(context){
+        super.draw(context);
+        context.beginPath();
+        context.moveTo(this.x + this.width/2, 0);
+        context.lineTo(this.x + this.width/2, this.y + 50);
+        context.stroke();
+    }
 }
